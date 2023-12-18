94db01c35f6c607054c8e36a5632909d49e011b0
Sachin76-NITH <20bec076@nith.ac.in>
2023-12-18 20:42:29 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,7 @@ mongoose.connect(process.env.CONNECTION_URL)
 .catch((error)=>console.log(error.message));
 
 const cors=require("cors")
-
+app.use(cors());
 const tweetRoutes = require("./routes/tweetRoutes.js")
 app.use(express.json({ limit: '10mb' }));
 app.use(bodyParser.json());
@@ -22,7 +22,7 @@ const authRoutes = require("./routes/AuthRoutes.js")
 
 
 
-app.use(cors());
+
 app.use(express.json())
 app.use(express.json({limit: '25mb'}));
 app.use(express.urlencoded({limit: '25mb', extended: true}));
